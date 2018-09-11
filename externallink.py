a4f809448018198a7ddd38e944a5b8f9cbed9c01
ehdals6 <ehdals614@naver.com>
2018-09-11 13:57:34 +0900

    Add files via upload

---
+++ externallink.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib import urlopen
+import re
+
+pages = set()
+
+def getInternalLinks(bsObj, includeUrl):
+    internalLinks = []
+    for link in bsObj.findAll("a", href=re.compile("^(/|.*"+includeUrl+")")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in internalLinks:
+                internalLinks.append(link.attrs['href'])
+    return internalLinks
+ 
+def getExternalLinks(bsObj, excludeUrl):
+    externalLinks = []
+    for link in bsObj.findAll("a", href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
+
+ 
+def splitAddress(address):
+    addressParts = address.replace("http://", "").split("/")
+    return addressParts
+
+allExtLinks = set()
+allIntLinks = set()
+
+def getAllExternalLinks(siteUrl):
+    html = urlopen(siteUrl)
+    bsObj = bs(html, "html.parser")
+    internalLinks = getInternalLinks(bsObj,splitAddress(domain)[0])
+    externalLinks = getExternalLinks(bsObj,splitAddress(domain)[0])
+ 
+    for link in externalLinks:
+        if link not in allExtLinks:
+            allExtLinks.add(link)
+            print(link)
+
+domain = 'http://zlal32teyptf4tvi.onion'
+getAllExternalLinks(domain)
+
