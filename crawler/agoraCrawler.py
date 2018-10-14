6095e351386e9120d41437827487ee62be5a8d16
Ungmotti <kyw268248@gmail.com>
2018-10-14 19:34:07 +0900

    all contents crawling

--- crawler/agoraCrawler.py
+++ crawler/agoraCrawler.py
@@ -11,45 +11,20 @@ agora='http://c2djzrn6qx6kupkn.onion/'
 
 with requests.Session() as s:
     agora = cr.Site(agora)
-    ss, soup = agora.staticGet(s, agora.stem)[0], agora.staticGet(s, agora.stem)[2]
-    messages = soup.find_all("div", {"class": "message"})
-    labels = soup.find_all("label")
-    ids = soup.find_all("span", {"class": "reflink"})
-    for id , label, message in zip(ids, labels, messages):
-        posterman = label.find("span", {"class": "postername"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "postername"}) is not None else None
-        filetitle = label.find("span", {"class": "filetitle"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "filetitle"}) is not None else None
-        for lab in label("span"):
-            lab.decompose()
-        mid = id.find_all('a')[-1].get_text()
-        date = label.get_text().encode('iso-8859-1').decode('utf-8').strip('\n').strip('  ')
-        ms = message.get_text().encode('iso-8859-1').decode('utf-8').strip('\n')
-    ss1, soup1 = agora.staticGet(s, agora.stem + "1.html")[0],agora.staticGet(s, agora.stem + "1.html")[2]
-    print(ss1 == ss)
-    print(soup1 == soup)
+    for i in range(1,862):
+        s, soup = agora.staticGet(s, agora.stem+ "/{}.html".format(i))[0], agora.staticGet(s, agora.stem + "/{}.html".format(i))[2]
+        messages = soup.find_all("div", {"class": "message"})
+        labels = soup.find_all("label")
+        ids = soup.find_all("span", {"class": "reflink"})
+        for id , label, message in zip(ids, labels, messages):
+            posterman = label.find("span", {"class": "postername"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "postername"}) is not None else None
+            filetitle = label.find("span", {"class": "filetitle"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "filetitle"}) is not None else None
+            for lab in label("span"):
+                lab.decompose()
+            mid = id.find_all('a')[-1].get_text()
+            date = label.get_text().encode('iso-8859-1').decode('utf-8').strip('\n').strip('  ')
+            ms = message.get_text().encode('iso-8859-1').decode('utf-8').strip('\n')
 
-'''
-        print("Message id : {}\n"
-              "postername : {}\n"
-              "title      : {}\n"
-              "Date       : {}\n"
-              "Message    : {}\n".format(mid,
-                                        posterman,
-                                        filetitle,
-                                        date,
-                                         ms))
-                                        #label.get_text().encode('iso-8859-1').decode('utf-8').strip('\n').strip('  '),
-                                        #message.get_text().encode('iso-8859-1').decode('utf-8').strip('\n')))
-'''
-'''
-    s.proxies = {}
-    s.proxies['http'] = 'socks5h://localhost:9050'
-    s.proxies['https'] = 'socks5h://localhost:9050'
-    req = s.get(agora, headers = {'Accept-Language' : 'utf-8, iso-8859-1;q=0.5, *;q=0.1'})
-    html = req.text
-    header = req.headers
-    status = req.status_code
-    soup = bs(html, 'html.parser')
-'''
 
 '''
     data = OrderedDict()
