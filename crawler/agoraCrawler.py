5c18af3a84b37b21e2653d7b7f458fa8d21a25a7
Ungmotti <kyw268248@gmail.com>
2018-10-14 19:47:17 +0900

    Write File

--- crawler/agoraCrawler.py
+++ crawler/agoraCrawler.py
@@ -8,23 +8,24 @@ import Crawler as cr
 agora='http://c2djzrn6qx6kupkn.onion/'
 #datas = list()
 
+with open('/home/kyw/highkorea/agora.txt', 'w+') as wf:
 
-with requests.Session() as s:
-    agora = cr.Site(agora)
-    for i in range(1,862):
-        s, soup = agora.staticGet(s, agora.stem+ "/{}.html".format(i))[0], agora.staticGet(s, agora.stem + "/{}.html".format(i))[2]
-        messages = soup.find_all("div", {"class": "message"})
-        labels = soup.find_all("label")
-        ids = soup.find_all("span", {"class": "reflink"})
-        for id , label, message in zip(ids, labels, messages):
-            posterman = label.find("span", {"class": "postername"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "postername"}) is not None else None
-            filetitle = label.find("span", {"class": "filetitle"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "filetitle"}) is not None else None
-            for lab in label("span"):
-                lab.decompose()
-            mid = id.find_all('a')[-1].get_text()
-            date = label.get_text().encode('iso-8859-1').decode('utf-8').strip('\n').strip('  ')
-            ms = message.get_text().encode('iso-8859-1').decode('utf-8').strip('\n')
-
+    with requests.Session() as s:
+        agora = cr.Site(agora)
+        for i in range(1,862):
+            s, soup = agora.staticGet(s, agora.stem+ "/{}.html".format(i))[0], agora.staticGet(s, agora.stem + "/{}.html".format(i))[2]
+            messages = soup.find_all("div", {"class": "message"})
+            labels = soup.find_all("label")
+            ids = soup.find_all("span", {"class": "reflink"})
+            for id , label, message in zip(ids, labels, messages):
+                posterman = label.find("span", {"class": "postername"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "postername"}) is not None else None
+                filetitle = label.find("span", {"class": "filetitle"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "filetitle"}) is not None else None
+                for lab in label("span"):
+                    lab.decompose()
+                mid = id.find_all('a')[-1].get_text()
+                date = label.get_text().encode('iso-8859-1').decode('utf-8').strip('\n').strip('  ')
+                ms = message.get_text().encode('iso-8859-1').decode('utf-8').strip('\n')
+                wf.write(ms)            
 
 '''
     data = OrderedDict()
