3f143d9e77c05ff6673c87e9ece94d8c4bcc342d
Ungmotti <kyw268248@gmail.com>
2018-10-14 19:33:16 +0900

    All contents Crawling only content

--- crawler/highkoreaThesis.py
+++ crawler/highkoreaThesis.py
@@ -13,85 +13,62 @@ LOGIN_INFO = {
     'username' : ID,
     'password' : passwd
 }
-sIDpat= re.compile('sid=(.*?);')
 
-with requests.session() as s:
-    highkorea = cr.Site(highkoreaURL)
-    s, soup = highkorea.staticGet(s, loginPage)[0], highkorea.staticGet(s, loginPage)[2]
+totaldata=[]
 
-    # Login
-    sid = soup.find('input', {'name': 'sid'})
-    redirect = soup.find('input', {'name': 'redirect'})
-    login = soup.find('input', {'name': 'login'})
-    LOGIN_INFO = dict(LOGIN_INFO, **{'sid': sid['value']})
-    LOGIN_INFO = dict(LOGIN_INFO, **{'redirect': redirect['value']})
-    LOGIN_INFO = dict(LOGIN_INFO, **{'login': login['value']})
-    s = highkorea.staticPost(s, loginPage, LOGIN_INFO)[0]
+with open('/home/kyw/highkorea/highkoreathesis.txt', 'w') as wf:
 
-    # Main Page
-    s, soup = highkorea.staticGet(s, highkorea.stem)[0], highkorea.staticGet(s, highkorea.stem)[2]
-    mainCategory = soup.find_all("a", {"class": "forumtitle"})
-    for category in mainCategory:
-        print("Forum : {}".format(category.text))
+    with requests.session() as s:
+        highkorea = cr.Site(highkoreaURL)
+        s, soup = highkorea.staticGet(s, loginPage)[0], highkorea.staticGet(s, loginPage)[2]
+
+        # Login
+        sid = soup.find('input', {'name': 'sid'})
+        redirect = soup.find('input', {'name': 'redirect'})
+        login = soup.find('input', {'name': 'login'})
+        LOGIN_INFO = dict(LOGIN_INFO, **{'sid': sid['value']})
+        LOGIN_INFO = dict(LOGIN_INFO, **{'redirect': redirect['value']})
+        LOGIN_INFO = dict(LOGIN_INFO, **{'login': login['value']})
+        s = highkorea.staticPost(s, loginPage, LOGIN_INFO)[0]
+
+        # Main Page
+        s, soup = highkorea.staticGet(s, highkorea.stem)[0], highkorea.staticGet(s, highkorea.stem)[2]
+        mainforums = soup.find_all("a", {"class": "forumtitle"})
+
+        for mainforum in mainforums:
+            print("Forum : {}".format(mainforum.text))
+            forumUrl = mainforum.get('href')
+            s, soup = highkorea.staticGet(s, highkorea.stem + forumUrl.strip('.'))[0], highkorea.staticGet(s, highkorea.stem + forumUrl.strip('.'))[2]
+            titles = soup.find_all("a", {"class": "topictitle"})
+
+            for title in titles:
+                titleURL = title.get('href')
+                s, soup = highkorea.staticGet(s, highkorea.stem + titleURL.strip('.'))[0], highkorea.staticGet(s, highkorea.stem + titleURL.strip('.'))[2]
+                contents = soup.find_all("div", {"class": "content"})
+                for content in contents:
+                    ct = content.get_text()
+                    wf.write(ct)
+
+
+  #         authors = soup.find_all("a",{"class": "username-coloured"})
+
+    #        for author, title in zip(authors, titles):
+
+                '''
+                data = dict()
+                data['author'] = author
+                data['title'] = title
+                data['content'] = []
+'''
+#                    data['content'].append(ct)
+#                totaldata.append(data)
 
 '''
-    LOGIN_INFO['sid'] = sid
-
-    r = s.post(loginPage, data= LOGIN_INFO)
-
-    html = r.text
-
-    soup = bs(html, 'html.parser')
-
-    print(soup)
-'''
-'''
-    req = s.get(highkorea)
-    
-    html = req.text
-
-    status = req.status_code
-
-    soup = bs(html, 'html.parser')
-
-    mainCategory = soup.find_all("a", {"class": "topictitle"})
-    for category in mainCategory:
-        print("Forum : {}".format(category.text))
+        authors = soup.find_all("a",{"class": "username-coloured"})
 '''
 
-'''
-    cookie = r.headers['Set-Cookie']
-    m = sIDpat.search(cookie)
-    r = s.get(highkorea + '&sid=' + m.group(1))
-    print(cookie == r.headers['Set-Cookie'])
+        #ddd= soup.find("dd", {"class":"lastpost"})
+        #for title, author in zip(titles, authors):
+        #    print("{} : {}".format(title.text, author.text))
 
-'''
-
-
-
-'''
-    header = {'Server': 'nginx',
-                'Date': 'Thu, 11 Oct 2018 02:03:33 GMT',
-                'Content-Type': 'text/html; charset=UTF-8',
-                'Transfer-Encoding': 'chunked',
-                'Connection': 'keep-alive',
-                'X-Powered-By': 'PHP/5.5.9-1ubuntu4.9',
-                'Set-Cookie':'phpbb3_d7j2o_u=1; '
-                            'expires=Fri, 11-Oct-2019 02:03:33 GMT; '
-                            'path=/; domain=highkorea5ou4wcy.onion; '
-                            'HttpOnly, phpbb3_d7j2o_k=; '
-                            'expires=Fri, 11-Oct-2019 02:03:33 GMT; '
-                            'path=/; domain=highkorea5ou4wcy.onion; '
-
-                            'HttpOnly, phpbb3_d7j2o_sid=d8054f73806238e5c03b790abb866065; '
-                            'expires=Fri, 11-Oct-2019 02:03:33 GMT; '
-                            'path=/; domain=highkorea5ou4wcy.onion; '
-
-                            'HttpOnly',
-
-                'Cache-Control': 'private, no-cache="set-cookie"',
-                'Expires': '0',
-                'Pragma': 'no-cache',
-                'Content-Encoding': 'gzip',
-                'Vary': 'Accept-Encoding'}
-'''
+#cr.mkjson(totaldata, '/home/kyw/json_datas', '20181014_thesis_highkorea.json')
