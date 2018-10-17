bb8a45dc6acb3578e7091aca8a654195fad64720
Ungmotti <kyw268248@gmail.com>
2018-10-18 01:40:31 +0900

    modified range part

--- crawler/agoraCrawler.py
+++ crawler/agoraCrawler.py
@@ -87,9 +87,9 @@ if __name__=='__main__':
     present_link = get_links()
     new_html = compare_number(present_link.strip('.html'), output)
     pool = Pool(processes=4) # 4개의 프로세스를 사용합니다.
-    pool.map(agoraMultiCrawler, range(new_html)) # get_contetn 함수를 넣어줍시다.
-    print("--- %s seconds ---" % (time.time() - start_time))
+    pool.map(agoraMultiCrawler, range(new_html+1)) # get_contetn 함수를 넣어줍시다.
 #    agoraCrawler(new_html)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
