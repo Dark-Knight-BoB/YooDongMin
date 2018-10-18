ae8bb2ca02b70bb579767b262fb4b2b7ddbdc6ba
Ungmotti <kyw268248@gmail.com>
2018-10-18 13:58:58 +0900

    multiprocessing and Deviding into functions

--- crawler/highkoreaThesis.py
+++ crawler/highkoreaThesis.py
@@ -1,26 +1,18 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import Crawler as cr
-from pprint import pprint
-from time import sleep
-import re
-loginPage = 'http://highkorea5ou4wcy.onion/ucp.php?mode=login'
-ID = 'michin'
-passwd = 'michin'
-highkoreaURL='http://highkorea5ou4wcy.onion'
+import time
+from multiprocessing import Pool
+from itertools import repeat
 
-LOGIN_INFO = {
-    'username' : ID,
-    'password' : passwd
-}
 
-totaldata=[]
-
-with open('/home/kyw/highkorea/highkoreathesis.txt', 'w') as wf:
-
+#with open('/home/kyw/highkorea/highkoreathesis.txt', 'w') as wf:
+def highkorealogin(login_info, object):
+    LOGIN_INFO=login_info
+    highkorea = object
     with requests.session() as s:
-        highkorea = cr.Site(highkoreaURL)
-        s, soup = highkorea.staticGet(s, loginPage)[0], highkorea.staticGet(s, loginPage)[2]
+        tup = highkorea.staticGet(s, loginPage)
+        s, soup = tup[0], tup[2]
 
         # Login
         sid = soup.find('input', {'name': 'sid'})
@@ -30,45 +22,75 @@ with open('/home/kyw/highkorea/highkoreathesis.txt', 'w') as wf:
         LOGIN_INFO = dict(LOGIN_INFO, **{'redirect': redirect['value']})
         LOGIN_INFO = dict(LOGIN_INFO, **{'login': login['value']})
         s = highkorea.staticPost(s, loginPage, LOGIN_INFO)[0]
+    return s, highkorea
+    # Main Page
 
-        # Main Page
-        s, soup = highkorea.staticGet(s, highkorea.stem)[0], highkorea.staticGet(s, highkorea.stem)[2]
-        mainforums = soup.find_all("a", {"class": "forumtitle"})
+def getForums(session, object, url):
+    highkorea = object
+    s = session
+    tup = highkorea.staticGet(s, url)
+    s, soup = tup[0], tup[2]
 
-        for mainforum in mainforums:
-            print("Forum : {}".format(mainforum.text))
-            forumUrl = mainforum.get('href')
-            s, soup = highkorea.staticGet(s, highkorea.stem + forumUrl.strip('.'))[0], highkorea.staticGet(s, highkorea.stem + forumUrl.strip('.'))[2]
-            titles = soup.find_all("a", {"class": "topictitle"})
-
-            for title in titles:
-                titleURL = title.get('href')
-                s, soup = highkorea.staticGet(s, highkorea.stem + titleURL.strip('.'))[0], highkorea.staticGet(s, highkorea.stem + titleURL.strip('.'))[2]
-                contents = soup.find_all("div", {"class": "content"})
-                for content in contents:
-                    ct = content.get_text()
-                    wf.write(ct)
+    forums = soup.find_all("a", {"class": "forumtitle"})
+    forumtitles = [forum.text for forum in forums]
+    forumURLs=[forum.get('href') for forum in forums]
+    return s, forumtitles, forumURLs
 
 
-  #         authors = soup.find_all("a",{"class": "username-coloured"})
-
-    #        for author, title in zip(authors, titles):
-
-                '''
-                data = dict()
-                data['author'] = author
-                data['title'] = title
-                data['content'] = []
-'''
-#                    data['content'].append(ct)
-#                totaldata.append(data)
-
-'''
+def getTitles(session, object, forumUrl):
+    highkorea = object
+    s = session
+    forumurl = highkorea.stem + forumUrl.strip('.')
+    tup = highkorea.staticGet(s, forumurl)
+    s, soup = tup[0], tup[2]
+    #lastpage = soup.find('span', {"class":"page-dots"}).next_element
+    titles = soup.find_all("a", {"class": "topictitle"})
+    print(titles)
+    #print(lastpage)
+    '''
+    for title in titles:
+        titleURL = title.get('href')
+        s, soup = highkorea.staticGet(s, highkorea.stem + titleURL.strip('.'))[0], highkorea.staticGet(s, highkorea.stem + titleURL.strip('.'))[2]
+        contents = soup.find_all("div", {"class": "content"})
         authors = soup.find_all("a",{"class": "username-coloured"})
-'''
+        for content in contents:
+            ct = content.get_text()
+    '''
 
-        #ddd= soup.find("dd", {"class":"lastpost"})
-        #for title, author in zip(titles, authors):
-        #    print("{} : {}".format(title.text, author.text))
+#http: // highkorea5ou4wcy.onion / viewforum.php?f = 34 & start = 23
+
+if __name__=='__main__':
+    loginPage = 'http://highkorea5ou4wcy.onion/ucp.php?mode=login'
+    mainpage = 'http://highkorea5ou4wcy.onion'
+
+    ID = 'michin'
+    passwd = 'michin'
+
+    LOGIN_INFO = {
+        'username': ID,
+        'password': passwd
+    }
+
+    start_time = time.time()
+    print(time.time())
+#    totaldata = []
+    highkorea = cr.Site(mainpage)
+
+    print(time.time())
+    # present_link = get_links()
+    # new_html = compare_number(present_link.strip('.html'), output)
+    session = highkorealogin(LOGIN_INFO, highkorea)[0]
+    print(time.time())
+    tup = getForums(session, highkorea, highkorea.stem)
+    #print(tup[1])
+#    for forum in tup[2]:
+#        getTitles(tup[0], highkorea, forum)
+    s = tup[0]
+
+    pool = Pool(processes=4) # 4개의 프로세스를 사용합니다.
+    pool.starmap(getTitles, zip(repeat(s), repeat(highkorea), tup[2]))
+#    agoraCrawler(new_html)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
 
 #cr.mkjson(totaldata, '/home/kyw/json_datas', '20181014_thesis_highkorea.json')
