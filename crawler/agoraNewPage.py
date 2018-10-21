37d505b547442e40edfbd0e3553ad440ed99c22a
Ungmotti <kyw268248@gmail.com>
2018-10-21 23:49:17 +0900

    Modified Name

--- crawler/agoraNewPage.py
+++ crawler/agoraNewPage.py
@@ -7,13 +7,14 @@ from collections import OrderedDict
 import subprocess
 import re
 import time
+import datetime
 #from pyvirtualdisplay import Display1
 #from selenium import webdriver
 #datas = list()
 #with open('/home/kyw/highkorea/agora.txt', 'w+') as wf:
 
 
-def get_links():
+def get_link():
     with open('/home/kyw/agoraHTMLnumber/agoraHTMLnumber.txt', 'a') as wf:
         agora = 'http://c2djzrn6qx6kupkn.onion/'
         with requests.Session() as s:
@@ -31,7 +32,7 @@ def compare_number(present_link, last_link):
     except ValueError:
         new_html = int(present_link) - 0
     return new_html
-
+'''
 def agoraCrawler(new_html):
     with requests.Session() as s:
         agora = 'http://c2djzrn6qx6kupkn.onion/'
@@ -56,22 +57,30 @@ def agoraCrawler(new_html):
                 date = label.get_text().encode('iso-8859-1').decode('utf-8').strip('\n').strip('  ')
                 ms = message.get_text().encode('iso-8859-1').decode('utf-8').strip('\n')
                 print(posterman)
+'''
 
 def agoraMultiCrawler(new_html):
+
     with requests.Session() as s:
+
+        return_data = OrderedDict()
         agora = 'http://c2djzrn6qx6kupkn.onion/'
         agora = cr.Site(agora)
-        i = new_html
-        if i == 0:
-            tup = agora.staticGet(s, agora.stem)
-            s, soup = tup[0], tup[2]
-        else:
-            tup = agora.staticGet(s, agora.stem + "/{}.html".format(i))
-            s, soup = tup[0], tup[2]
+
+        # if i == 0:
+        #     tup = agora.staticGet(s, agora.stem)
+        #     s, html, soup = tup[0], tup[1].text, tup[2]
+        tup = agora.staticGet(s, agora.stem + "/{}.html".format(new_html))
+        s, html, soup = tup[0], tup[1].text, tup[2]
         messages = soup.find_all("div", {"class": "message"})
         labels = soup.find_all("label")
+
         ids = soup.find_all("span", {"class": "reflink"})
+
+        content_data = list()
+
         for id , label, message in zip(ids, labels, messages):
+            temp_data = OrderedDict()
             posterman = label.find("span", {"class": "postername"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "postername"}) is not None else None
             filetitle = label.find("span", {"class": "filetitle"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "filetitle"}) is not None else None
             for lab in label("span"):
@@ -79,27 +88,64 @@ def agoraMultiCrawler(new_html):
             mid = id.find_all('a')[-1].get_text()
             date = label.get_text().encode('iso-8859-1').decode('utf-8').strip('\n').strip('  ')
             ms = message.get_text().encode('iso-8859-1').decode('utf-8').strip('\n')
+            temp_data['author']=posterman
+            temp_data['title']=filetitle
+            temp_data['id']=mid
+            temp_data['date']=date
+            temp_data['message']=ms
+            content_data.append(temp_data)
+        return_data['html'] = html
+        return_data['content'] = content_data
+        return_data['url'] = tup[1].url
+        return return_data
+
+
+def agoraStemCrawler():
+    with requests.Session() as s:
+
+        return_data = OrderedDict()
+        agora = 'http://c2djzrn6qx6kupkn.onion/'
+        agora = cr.Site(agora)
+        tup = agora.staticGet(s, agora.stem)
+        s, html, soup = tup[0], tup[1].text, tup[2]
+        messages = soup.find_all("div", {"class": "message"})
+        labels = soup.find_all("label")
+
+        ids = soup.find_all("span", {"class": "reflink"})
+
+        content_data = list()
+
+        for id , label, message in zip(ids, labels, messages):
+            temp_data = OrderedDict()
+            posterman = label.find("span", {"class": "postername"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "postername"}) is not None else None
+            filetitle = label.find("span", {"class": "filetitle"}).get_text().encode('iso-8859-1').decode('utf-8').strip('\n') if label.find("span", {"class": "filetitle"}) is not None else None
+            for lab in label("span"):
+                lab.decompose()
+            mid = id.find_all('a')[-1].get_text()
+            date = label.get_text().encode('iso-8859-1').decode('utf-8').strip('\n').strip('  ')
+            ms = message.get_text().encode('iso-8859-1').decode('utf-8').strip('\n')
+            temp_data['author']=posterman
+            temp_data['title']=filetitle
+            temp_data['id']=mid
+            temp_data['date']=date
+            temp_data['message']=ms
+            content_data.append(temp_data)
+        return_data['html'] = html
+        return_data['content'] = content_data
+        return_data['url'] = tup[1].url
+        return return_data
+
 
 
 if __name__=='__main__':
     start_time = time.time()
     output = subprocess.check_output(['tail', '-n 1', '/home/kyw/agoraHTMLnumber/agoraHTMLnumber.txt'], universal_newlines=True)
-    present_link = get_links()
+    present_link = get_link()
     new_html = compare_number(present_link.strip('.html'), output)
+    total_data=list()
+    data = agoraStemCrawler()
     pool = Pool(processes=4) # 4개의 프로세스를 사용합니다.
-    pool.map(agoraMultiCrawler, range(new_html+1)) # get_contetn 함수를 넣어줍시다.
- #   agoraCrawler(new_html)
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
-
-'''
-    data = OrderedDict()
-    data['id'] = id.find_all('a')[-1].get_text()
-    data['date'] = date.get_text()
-    data['message'] = message.get_text()
+    datas = pool.map(agoraMultiCrawler, range(new_html+1))
     datas.append(data)
-'''
-
-#cr.mkjson('/home/kyw/json_datas/', datas)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    cr.mkjson(datas, '/home/kyw/json_datas/agora', 'agora.json')
