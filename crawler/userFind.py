e8ae98d2b7573be5079a335375182fd1765168a3
Ungmotti <kyw268248@gmail.com>
2018-10-25 00:57:02 +0900

    first upload

---
+++ crawler/userFind.py
@@ -0,0 +1,11 @@
+import requests
+
+if __name__ == '__main__':
+    google = 'https://www.google.co.kr/search?q='
+    twitter = 'https://twitter.com/'
+    instagram = 'https://www.instagram.com/'
+    naverblog = 'https://blog.naver.com/'
+    daumblog = 'http://blog.daum.net/'
+    youtube = 'https://www.youtube.com/user/'
+    tistory = 'http://{}.tistory.com/'
+    uid = ''
